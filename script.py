1562a0cc103fb529054a54bb30848e383e1e22cf
Joseph Nguyen <joseph.t.nguyen99@gmail.com>
2023-08-28 23:15:08 +1000

    Code made more flexible

--- script.py
+++ script.py
@@ -5,28 +5,55 @@
 import serial       # allows access to serial ports 
 import pyautogui    # allows keyboard/mouse inputs
 
-signalLeft = b'A'   # move left
+signalLeft  = b'A'  # move left
 signalRight = b'D'  # move right
-signalDown = b'S'   # move down
-signalDrop = b'B'   # drop/spacebar
-signalRotateL = b'Z' # rotate block left
-signalRotateR = b'U' # rotate block right
+signalDown  = b'S'  # move down
+signalUp    = b'W'  # move up
+signalB1    = b'B'  # Optional button 
+signalB2    = b'Z'  # Optional button 
+signalB3    = b'U'  # Optional button
 
 comport = "COM7"
 ser = serial.Serial(comport, 9600)
 
+# Dictionary for default joystick signals
+signal_actions = {
+    signalLeft: 'left',
+    signalRight: 'right',
+    signalDown: 'down',
+    signalUp: 'up'
+}
+
+buttons_required = False
+invalid_input = True
+
+# Check if buttons are required for the program
+while invalid_input:
+    buttons_req = input("Buttons required? Y/N: ")
+    if buttons_req == 'Y':
+        buttons_required = True
+        invalid_input = False
+    elif buttons_req == 'N':
+        invalid_input = False
+    else:
+        print("Invalid Input")
+
+# only if buttons are required, let user select functionality
+if buttons_required:
+    signal_actions[signalB1] = input("Configure button 1: ")
+    signal_actions[signalB2] = input("Configure button 2: ")
+    signal_actions[signalB3] = input("Configure button 3: ")
+else:
+    # Defaults to 'space' if button is not required
+    signal_actions[signalB1] = ' '
+    signal_actions[signalB2] = ' '
+    signal_actions[signalB3] = ' '
+
+# Main loop
 while True:
     signal = ser.read() # Read the signal once (signal might change midway through loop)
-    if signal == signalLeft:
-        pyautogui.press('left')
-    elif signal == signalRight:
-        pyautogui.press('right')
-    elif signal == signalDown:
-        pyautogui.press('down')
-    elif signal == signalDrop:
-        pyautogui.press('space')
-    elif signal == signalRotateL:
-        pyautogui.press('z')
-    elif signal == signalRotateR:
-        pyautogui.press('up')
+    action = signal_actions.get(signal) # get corresponding signal
+    if action:
+        pyautogui.press(action)
 
+    
